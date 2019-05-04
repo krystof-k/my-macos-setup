49ab7460d7f028627caa548d44eb0c77aef91fc8
Kryštof Korb <krystof@korb.cz>
2019-05-04 21:36:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,10 @@ sudo scutil --set LocalHostName 'krystofs-macbook-pro'
 # Show battery percentage
 defaults write com.apple.menuextra.battery ShowPercent YES
 
-
+# Enable tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write 'Apple Global Domain' com.apple.mouse.tapBehavior 1
+
+# Enable App Exposé gesture
+defaults write com.apple.dock showAppExposeGestureEnabled -bool true
