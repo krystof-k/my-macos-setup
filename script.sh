17dd72419a959a6bac7c029be4a9cf1dc764dda4
Kryštof Korb <krystof@korb.cz>
2019-05-04 20:30:16 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+
+# Rename computer
+sudo scutil --set ComputerName 'Kryštof's MacBook Pro'
+sudo scutil --set LocalHostName 'krystofs-macbook-pro'
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent YES
+
+
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
