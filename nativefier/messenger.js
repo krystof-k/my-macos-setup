a85513bcfad4900f902dba68df0d46d3a666381e
Kryštof Korb <krystof@korb.cz>
2019-12-08 13:56:04 +0100

    Create messenger.js

---
+++ nativefier/messenger.js
@@ -0,0 +1,15 @@
+var css = `._6-xk,
+._673w._6ynl {
+  -webkit-app-region: drag;
+  margin-top: 12px;
+}
+
+._6-xk ._6ymu._7iq4,
+._6-xk a,
+._673w._6ynl ._fl2._6ymr {
+  -webkit-app-region: no-drag;
+}`
+
+var style = document.createElement('style');
+style.innerHTML = css;
+document.head.appendChild(style);
