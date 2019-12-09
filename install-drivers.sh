47c057d50b0d05b24ab9ca67d46dfed910011dc0
Kryštof Korb <krystof@korb.cz>
2019-12-09 23:25:55 +0100

    Update install-drivers.sh

--- install-drivers.sh
+++ install-drivers.sh
@@ -2,7 +2,7 @@
 xcode-select -p
 if [[ $? != 0 ]]; then
   xcode-select --install
-  printf "\n\nWaiting for Xcode Command Line Tools to be installed."
+  printf "\nWaiting for Xcode Command Line Tools to be installed.\n"
   sleep 15
   read -p "Press enter after installation completes to continue."
 fi
@@ -18,13 +18,13 @@ brew update
 # Install drivers in the order from README.md
 
 # Logi Options
-printf "\n\nInstallation of Logi Options will continue."
+printf "\nInstallation of Logi Options will continue.\n"
 printf "Be aware of pop-up windows and special permissions to finish the installation."
 sleep 3
 brew cask install logitech-options
 
 # Logitech Control Center
-printf "\n\nInstallation of Logitech Control Center will continue."
+printf "\nInstallation of Logitech Control Center will continue.\n"
 printf "Be aware of pop-up windows and special permissions to finish the installation."
 sleep 3
 brew cask install logitech-control-center
