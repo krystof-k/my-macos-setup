4badef83ffb9f7213c0478cabc344e4ac745b7e0
Kryštof Korb <krystof@korb.cz>
2019-12-14 21:02:03 +0100

    Update finder.sh

--- preferences/finder.sh
+++ preferences/finder.sh
@@ -1,23 +1,23 @@
+#!/usr/bin/env bash
 
+# Preferences
 
-# Display extensions
-defaults write -g AppleShowAllExtensions -bool true
-
-# Display path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Show Quit Finder menu option
-defaults write com.apple.finder QuitMenuItem -bool true
-
-# Uncollapse places and devices in sidebar
-defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
-defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
-
+# Don't show removable media on desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
 # Show home folder as default when opening new window
 defaults write com.apple.finder NewWindowTarget -string PfHm
 defaults write com.apple.finder NewWindowTargetPath -string '~/.'
 
+# Display home folder in sidebar
+echo " Manually show home folder in sidebar."
+
+# Display extensions
+defaults write -g AppleShowAllExtensions -bool true
+
+# Don't warn when changing extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Empty the trash after 30 days
 defaults write com.apple.finder FXRemoveOldTrashItems -bool true
@@ -25,19 +25,25 @@ defaults write com.apple.finder FXRemoveOldTrashItems -bool true
 # Keep folders on top when sorting by name
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Don't show removable media on desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+# Set current folder as a default search scope
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf
 
-# Don't warn when changing extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+
+# View
 
 # Set column layout as default
 defaults write com.apple.finder FXPreferredViewStyle -string clmv
+
+# Display path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
 # Set grouping by date added as default
 defaults write com.apple.finder FXPreferredGroupBy -string 'Date Added'
 
 
-# Set current folder as a default search scope
-defaults write com.apple.finder FXDefaultSearchScope -string SCcf
+
+# Other
+
+# Show Quit Finder menu option
+defaults write com.apple.finder QuitMenuItem -bool true
