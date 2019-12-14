cf42262b4fcfe07f6b22443526693d4a44e6c19a
Kryštof Korb <krystof@korb.cz>
2019-12-14 21:30:48 +0100

    Update setup-preferences.sh

--- setup-preferences.sh
+++ setup-preferences.sh
@@ -1,16 +1,13 @@
-
-
-
-
-
-
-# Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+#!/usr/bin/env bash
 
 # Disable startup chime
 sudo nvram SystemAudioVolume=" "
 
+# Disable Caps Lock
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000000}]}'
+
+# Show battery percentage in the menu bar
+defaults write com.apple.menuextra.battery ShowPercent -string YES
 
 # Expand save and print panels by default
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
@@ -21,42 +18,12 @@ defaults write -g PMPrintingExpandedStateForPrint2 -bool true
 # Don't save to cloud by default
 defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
 
-
-
-
-
-
-
-mkdir -p ~/Screenshots
-defaults write com.apple.screencapture location ~/Screenshots
-
-defaults write com.apple.screencapture disable-shadow -bool true
-
-
-
-echo "Speeding up wake from sleep to 24 hours from an hour"
-# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
-sudo pmset -a standbydelay 86400
-
 # Enable AirDrop over Ethernet and on Unsupported Macs
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Not sure it works
-
-
-
-
-# disable caps
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000000}]}'
-
-
-
-
-
-
-
-# Others
-
-# Show battery percentage in the menu bar
-defaults write com.apple.menuextra.battery ShowPercent -string YES
+# Prolong standby delay to 5 hours – faster wake
+sudo pmset -a standbydelay 18000
