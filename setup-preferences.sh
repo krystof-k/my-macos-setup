8eacde27521fefc9f0466da7a104c624a9068cc8
Kryštof Korb <krystof@korb.cz>
2019-12-14 11:11:12 +0100

    Update setup-preferences.sh

--- setup-preferences.sh
+++ setup-preferences.sh
@@ -2,45 +2,10 @@
 sudo systemsetup -setcomputername "Kryštof's MacBook Pro"
 sudo systemsetup -setlocalsubnetname 'krystofs-macbook-pro'
 
-# Show battery percentage
-defaults write com.apple.menuextra.battery ShowPercent -string YES
 
-# Enable tap to click
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults write -g com.apple.mouse.tapBehavior -int 1
 
-# Enable App Exposé gesture
-defaults write com.apple.dock showAppExposeGestureEnabled -bool true
 
-# Don't automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
 
-# Set Windows-like keyboard shortcut for switching app windows
-/usr/libexec/PlistBuddy -c "Delete :AppleSymbolicHotKeys:27 dict" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27 dict" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:enabled bool 1" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value dict" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value:type string standard" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value:parameters array" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value:parameters:0 integer 65535" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value:parameters:1 integer 48" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-/usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:27:value:parameters:2 integer 524288" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-
-# Empty the trash after 30 days
-defaults write com.apple.finder FXRemoveOldTrashItems -bool true
-
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
-
-# Display path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Show Quit Finder menu option
-defaults write com.apple.finder QuitMenuItem -bool true
-
-# Display extensions
-defaults write -g AppleShowAllExtensions -bool true
 
 # Disable autocorrect, autocapitalization, etc.
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
@@ -56,14 +21,6 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Disable startup chime
 sudo nvram SystemAudioVolume=" "
 
-# Set dock size
-defaults write com.apple.dock tilesize -int 48
-
-# Lock dock size
-defaults write com.apple.dock size-immutable -bool true
-
-# Autohide dock
-defaults write com.apple.dock autohide -bool true
 
 # Expand save and print panels by default
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
@@ -74,11 +31,7 @@ defaults write -g PMPrintingExpandedStateForPrint2 -bool true
 # Don't save to cloud by default
 defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
 
-# Set current folder as a default search scope
-defaults write com.apple.finder FXDefaultSearchScope -string SCcf
 
-# Set default sidebar size to small
-defaults write -g NSTableViewDefaultSizeMode -int 1
 
 
 systemsetup -setnetworktimeserver ntp.nic.cz
@@ -126,3 +79,24 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 # Set recent items limit to 50
 defaults write NSGlobalDomain NSRecentDocumentsLimit -int 50
 defaults write NSGlobalDomain NSNavRecentPlacesLimit -int 50
+
+
+
+
+
+
+
+
+
+
+# Others
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -string YES
+
+
+
+# Display Bluetooth in menu bar
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -bool true
+/usr/libexec/PlistBuddy -c "Add :menuExtras: string '/System/Library/CoreServices/Menu Extras/Bluetooth.menu'" ~/Library/Preferences/com.apple.systemuiserver.plist
+
