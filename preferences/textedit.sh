cbb70bdfcc0cd75438fe429e67eb384ee7317810
Kryštof Korb <krystof@korb.cz>
2019-12-14 14:38:27 +0100

    Create textedit.sh

---
+++ preferences/textedit.sh
@@ -0,0 +1,9 @@
+"com.apple.TextEdit" =     {
+        CheckSpellingWhileTyping = 0;
+        CorrectSpellingAutomatically = 0;
+        RichText = 0;
+        SmartCopyPaste = 0;
+        SmartDashes = 0;
+        SmartQuotes = 0;
+        SmartSubstitutionsEnabledInRichTextOnly = 0;
+    };
