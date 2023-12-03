ad2986750049c8d7fac2967b917c572178c69947
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:55:18 +0100

    TODO Set up Docker

---
+++ setup/docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+message 'Open Docker, wait for the daemon to start and press any key to continue' 'step' 'prompt'
+sleep 2
+open -a Docker
+read -n 1 -s -r
+
+message 'Login to Docker container registry' 'substep'
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u $GITHUB_PACKAGES_TOKEN_USERNAME --password-stdin
