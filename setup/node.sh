d20c1fa815ca970a02510c1890285f9957dd39b3
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:45:42 +0100

    Set up Node.js

---
+++ setup/node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../utilities/message.sh"
+
+message 'Set up Node version manager' 'step'
+message "Currently using Node.js (`node --version`) at \``which node`\`" 'substep' 'info'
+message 'Add NVM to ~/.zprofile' 'substep'
+mkdir ~/.nvm
+echo '# Node version manager' >> ~/.zprofile
+echo 'export NVM_DIR=~/.nvm' >> ~/.zprofile
+echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.zprofile
+echo 'source $(brew --prefix nvm)/etc/bash_completion.d/nvm' >> ~/.zprofile
+echo '' >> ~/.zprofile
+message 'Reload .zprofile' 'substep'
+source ~/.zprofile
+message 'Install Node.js 20' 'substep'
+nvm install 20
+message "Currently using Node.js (`node --version`) at \``which node`\`" 'substep' 'info'
+
+message 'Install Yarn' 'step'
+npm install --global yarn
