23090eef3401926620183b236905c66f92eba943
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:50:08 +0100

    TODO Set up Python

---
+++ setup/python.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+# message 'Install Python packages' 'step'
+# pip3 install -r ./apps/python/requirements.txt
+# message 'Set up GitHub Packages authentication' 'step'
