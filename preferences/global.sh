cf8ab5484c61ff3f45ba4e2c18df459edc206bc4
Kryštof Korb <krystof@korb.cz>
2023-12-04 00:09:42 +0100

    Configure global preferences

---
+++ preferences/global.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../utilities/message.sh"
+
+message 'Set input preferences' 'step'
+
+message 'Disable autocorrect' 'substep'
+defaults write -g 'NSAutomaticSpellingCorrectionEnabled' -bool false
+
+message 'Disable automatic capitalization' 'substep'
+defaults write -g 'NSAutomaticCapitalizationEnabled' -bool false
+
+message 'Disable smart quotes' 'substep'
+defaults write -g 'NSAutomaticQuoteSubstitutionEnabled' -bool false
+
+message 'Disable smart dashes' 'substep'
+defaults write -g 'NSAutomaticDashSubstitutionEnabled' -bool false
+
+message 'Enable double spacebar period' 'substep'
+defaults write -g 'NSAutomaticPeriodSubstitutionEnabled' -bool true
+
+message 'Set other preferences' 'step'
+
+message 'Switch to dark mode automatically' 'substep'
+if defaults read -g 'AppleInterfaceStyle' > /dev/null 2>&1; then
+  defaults remove -g 'AppleInterfaceStyle'
+fi
+defaults write -g 'AppleInterfaceStyleSwitchesAutomatically' -bool true
+
+message 'Double click window header to minimize' 'substep'
+defaults write -g 'AppleActionOnDoubleClick' -string 'Minimize'
+
+message 'Show file extensions' 'substep'
+defaults write -g 'AppleShowAllExtensions' -bool true
+
+message 'Expand save and print panels' 'substep'
+defaults write -g 'NSNavPanelExpandedStateForSaveMode' -bool true
+defaults write -g 'PMPrintingExpandedStateForPrint' -bool true
+
+message 'Switch to a nearest NTP server' 'substep'
+sudo systemsetup -setnetworktimeserver ntp.nic.cz
+
+message 'Add ~/Git to Spotlight exclusions' 'substep'
+sudo /usr/libexec/PlistBuddy -c "Add :Exclusions:0 string '/Users/krystof-k/Git'" /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist
+sudo launchctl stop com.apple.metadata.mds
+sudo launchctl start com.apple.metadata.mds
+
+message 'Enable firewall' 'substep'
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw \
+  --setblockall off \
+  --setallowsigned on \
+  --setallowsignedapp on \
+  --setloggingmode on \
+  --setstealthmode off \
+  --setglobalstate on
+
+message 'Turn off display after 3 minutes' 'substep'
+sudo pmset -a displaysleep 3
+
+message 'Sleep after 3 minutes on battery' 'substep'
+sudo pmset -b sleep 3
+
+message 'Sleep after 30 minutes on power' 'substep'
+sudo pmset -c sleep 30
+
+message 'Manually set keyboard backlight limit to 30 seconds' 'substep' 'to-do'
+message 'Manually disable Caps Lock for all keyboards' 'substep' 'to-do'
+message 'Manually set ask for password delay to 5 seconds' 'substep' 'to-do'
+message 'Manually turn on encryption' 'substep' 'to-do'
--- setup.sh
+++ setup.sh
@@ -121,3 +121,6 @@ message 'Set up Ruby'
 
 message 'Set up Node.js'
 ./setup/node.sh
+
+message 'Configure global preferences'
+./preferences/global.sh
