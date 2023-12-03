894759782f58dce3ffc45adc03d2d8b9f9dd50c8
Kryštof Korb <krystof-k@Krystofs-MacBook-Pro.local>
2023-12-04 00:00:08 +0100

    TODO Configure Finder

--- preferences/apps/com.apple.finder.sh
+++ preferences/apps/com.apple.finder.sh
@@ -6,12 +6,26 @@ source "$(dirname $0)/../../utilities/message.sh"
 
 message 'Configure Finder' 'step'
 
+<<<<<<< HEAD
 message 'Set grouping by date added as default' 'substep'
 defaults write com.apple.finder 'FXPreferredGroupBy' -string 'Date Added'
 
 message 'Calculate folder sizes' 'substep'
 /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:ExtendedListViewSettingsV2:calculateAllSizes bool true' ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:ListViewSettings:calculateAllSizes bool true' ~/Library/Preferences/com.apple.finder.plist
+=======
+# TODO
+# Set grouping by date added as default
+defaults write com.apple.finder 'FXPreferredGroupBy' -string 'Date Added'
+
+# TODO
+message 'Calculate folder sizes' 'substep'
+/usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:ExtendedListViewSettingsV2:calculateAllSizes bool true' ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:ListViewSettings:calculateAllSizes bool true' ~/Library/Preferences/com.apple.finder.plist
+# finderkit dialogy z appek?
+# /usr/libexec/PlistBuddy -c 'Set :FK_StandardViewSettings:ExtendedListViewSettingsV2:calculateAllSizes true' ~/Library/Preferences/com.apple.finder.plist
+# /usr/libexec/PlistBuddy -c 'Set :FK_StandardViewSettings:ListViewSettings:calculateAllSizes true' ~/Library/Preferences/com.apple.finder.plist
+>>>>>>> 48916fb (TODO Configure Finder)
 
 message 'Set column view style as default' 'substep'
 defaults write com.apple.finder 'FXPreferredViewStyle' -string 'clmv'
@@ -34,12 +48,18 @@ defaults write com.apple.finder 'FXRemoveOldTrashItems' -bool true
 message 'Show quit menu option' 'substep'
 defaults write com.apple.finder 'QuitMenuItem' -bool true
 
+<<<<<<< HEAD
 message 'Show home folder for new windows' 'substep'
 defaults write com.apple.finder NewWindowTarget -string PfHm
 defaults write com.apple.finder NewWindowTargetPath -string '~/'
 
 message 'Manually display Library folder in home folder preferences' 'substep' 'to-do'
 message 'Manually show home folder in sidebar' 'substep' 'to-do'
+=======
+message 'Manually display Library folder in home folder preferences' 'substep' 'to-do'
+message 'Manually show home folder in sidebar' 'substep' 'to-do'
+
+>>>>>>> 48916fb (TODO Configure Finder)
 message 'Manually set recent items limit to 50' 'substep' 'to-do'
 
 # Desktop
@@ -55,3 +75,19 @@ defaults write com.apple.finder 'ShowRemovableMediaOnDesktop' -bool false
 
 message 'Restart Finder' 'substep'
 killall Finder
+<<<<<<< HEAD
+=======
+
+# TODO
+# message 'Show home folder for new windows' 'substep'
+# defaults write com.apple.finder NewWindowTarget -string PfHm && \
+# defaults write com.apple.finder NewWindowTargetPath -string '~/' && (killall Finder 2> /dev/null || true)
+
+# TODO
+# message "Don't save new documents to iCloud" 'substep'
+# defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false && (killall Finder 2> /dev/null || true)
+
+# TODO works
+# message 'Set sidebar icons to small' 'substep'
+# defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1 && (killall Finder 2> /dev/null || true)
+>>>>>>> 48916fb (TODO Configure Finder)
