d27da6716b87eac25c5ffa9a27f3b6226416a9f9
Kryštof Korb <krystof-k@Krystofs-MacBook-Pro.local>
2023-12-03 23:59:03 +0100

    TODO Configure dock

--- preferences/apps/com.apple.dock.sh
+++ preferences/apps/com.apple.dock.sh
@@ -17,6 +17,18 @@ defaults write com.apple.dock 'size-immutable' -bool true
 message 'Autohide dock' 'substep'
 defaults write com.apple.dock 'autohide' -bool true
 
+<<<<<<< HEAD
+=======
+# TODO
+# # + Speed up autohide
+# defaults write com.apple.dock autohide-delay -float 0
+# defaults write com.apple.dock autohide-time-modifier -float 0.5
+
+# TODO works
+# # + Clean dock apps
+# defaults delete com.apple.dock persistent-apps
+
+>>>>>>> 7df296b (TODO Configure dock)
 # Windows & desktops
 
 message 'Use scale effect when minimizing' 'substep'
@@ -25,14 +37,25 @@ defaults write com.apple.dock mineffect -string 'scale'
 message "Don't rearrange desktops" 'substep'
 defaults write com.apple.dock 'mru-spaces' -bool false
 
+<<<<<<< HEAD
 message 'Enable App Exposé gesture' 'substep'
+=======
+message "Enable App Exposé gesture" 'substep'
+>>>>>>> 7df296b (TODO Configure dock)
 defaults write com.apple.dock 'showAppExposeGestureEnabled' -bool true
 
 # Launchpad
 
+<<<<<<< HEAD
 message 'Set launchpad layout to 4 × 5' 'substep'
 defaults write com.apple.dock springboard-columns -int 4
 defaults write com.apple.dock springboard-rows -int 5
+=======
+# TODO
+# # + Set launchpad layout to 4 × 5
+# defaults write com.apple.dock springboard-columns -int 4
+# defaults write com.apple.dock springboard-rows -int 5
+>>>>>>> 7df296b (TODO Configure dock)
 
 message 'Restart dock' 'substep'
 killall Dock
