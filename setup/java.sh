e509e2e3b77826871470ca084ad67533f51f4b35
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:50:35 +0100

    TODO Set up Java

---
+++ setup/java.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+# message 'Configure Java' 'step'
+# echo 'export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"' >> ~/.zprofile
+# echo 'export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"' >> ~/.zshenv
