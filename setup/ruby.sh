3d6919dfeccd6bb33610ba827fdb1381919970ae
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:45:20 +0100

    Set up Ruby

---
+++ setup/ruby.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../utilities/message.sh"
+
+message 'Set up Ruby version manager' 'step'
+message "Currently using Ruby (`ruby --version`) at \``which ruby`\`" 'substep' 'info'
+message 'Add rbenv to ~/.zprofile' 'substep'
+echo 'eval "$(rbenv init -)"' >> ~/.zprofile
+message 'Reload .zprofile' 'substep'
+source ~/.zprofile
+rbenv install 3.2.2
+rbenv global 3.2.2
+message "Currently using Ruby (`ruby --version`) at \``which ruby`\`" 'substep' 'info'
