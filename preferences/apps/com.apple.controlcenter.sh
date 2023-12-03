ce78d154e0790aab089fb8a51b6c9f0535d477e5
Kryštof Korb <krystof-k@Krystofs-MacBook-Pro.local>
2023-12-03 23:51:03 +0100

    Configure control center

---
+++ preferences/apps/com.apple.controlcenter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+message 'Configure control center' 'step'
+
+message 'Always show sound control' 'substep'
+defaults write com.apple.controlcenter 'NSStatusItem Visible Sound' -bool true
+defaults -currentHost write com.apple.controlcenter 'Sound' -int 18
+
+message 'Always show Bluetooh' 'substep'
+defaults write com.apple.controlcenter 'NSStatusItem Visible Bluetooth' -bool true
+defaults -currentHost write com.apple.controlcenter 'Bluetooth' -int 2
