1b04cb91a0bf78cb6d70c0cb22ea3d93917b9853
Kryštof Korb <krystof@korb.cz>
2023-12-03 20:22:04 +0100

    Add authentication steps

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Just copy-paste this into your freshly installed Mac's terminal 🚀
 ```console
 /bin/bash -c "$(curl -fsSL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/krystof-k/my-macos-setup/main/setup.sh)"
 ```
+
+| Option | Description |
+| - | - |
+| `--skip-authentication` | Skip signing in, setting up SSH keys and other authentication steps |
--- setup.sh
+++ setup.sh
@@ -23,3 +23,33 @@ fi
 
 message 'Create a clean APFS snapshot' 'step'
 tmutil localsnapshot
+
+if [[ ! $@ =~ --skip-authentication ]]; then
+  message 'Please sign into the App Store manually and press any key to continue' 'step' 'prompt'
+  sleep 2
+  open -a "App Store"
+  read -n 1 -s -r
+
+  message 'Add private key' 'step'
+  message 'Enter absolute path to your private key' 'substep' 'prompt'
+  read PRIVATE_KEY_PATH
+  PRIVATE_KEY_FILENAME=`basename $PRIVATE_KEY_PATH`
+  message 'Move private key to `~/.ssh`' 'substep'
+  mkdir -p ~/.ssh
+  mv $PRIVATE_KEY_PATH ~/.ssh/$PRIVATE_KEY_FILENAME
+  chmod 600 ~/.ssh/$PRIVATE_KEY_FILENAME
+  message 'Add it to SSH agent' 'substep'
+  # -K option is deprecated in favor of --apple-use-keychain since macOS Monterey
+  ssh-add --apple-use-keychain ~/.ssh/$PRIVATE_KEY_FILENAME
+  message 'Add it to `~/.ssh/config`' 'substep'
+  tee -a ~/.ssh/config << END > /dev/null
+Host *
+  UseKeychain yes
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/$PRIVATE_KEY_FILENAME
+END
+  message "Export public key to \`~/.ssh/${PRIVATE_KEY_FILENAME%.pem}.pub\`" 'substep'
+  ssh-keygen -y -f ~/.ssh/$PRIVATE_KEY_FILENAME > ~/.ssh/${PRIVATE_KEY_FILENAME%.pem}.pub
+else
+  message 'Skipping authentication' 'info'
+fi
