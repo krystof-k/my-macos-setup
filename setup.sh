ed03c3a994143b62eb0da16483378fd628464f01
Kryštof Korb <krystof@korb.cz>
2023-12-17 23:14:58 +0100

    homebrew nicer zprofile

--- setup.sh
+++ setup.sh
@@ -74,9 +74,11 @@ if [[ ! $@ =~ --skip-homebrew-install ]]; then
   message 'Install Homebrew' 'step'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   message 'Add Homebrew to PATH' 'substep'
+  echo '# Homebrew' >> ~/.zprofile
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   message 'Set number of Homebrew CURL retries to 10' 'substep'
   echo 'HOMEBREW_CURL_RETRIES=10' >> ~/.zprofile
+  echo '' >> ~/.zprofile
   message 'Reload .zprofile' 'substep'
   source ~/.zprofile
 else
