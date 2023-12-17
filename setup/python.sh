1644ee3e44b60afa7c343109d65ef07f8e38669f
Kryštof Korb <krystof-k@Krystofs-MacBook-Pro.local>
2023-12-17 23:36:22 +0100

    fixup! TODO Set up Python

--- setup/python.sh
+++ setup/python.sh
@@ -2,8 +2,26 @@
 
 set -e # exit on any error
 
-source "$(dirname $0)/../../utilities/message.sh"
+source "$(dirname $0)/../utilities/message.sh"
 
-# message 'Install Python packages' 'step'
-# pip3 install -r ./apps/python/requirements.txt
-# message 'Set up GitHub Packages authentication' 'step'
+message 'Set up Python version manager' 'step'
+message "Currently using Python (`python3 --version`) at \``which python3`\`" 'substep' 'info'
+message 'Add pyenv to ~/.zprofile' 'substep'
+echo '# Python version manager' >> ~/.zprofile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zprofile
+echo '' >> ~/.zprofile
+message 'Reload .zprofile' 'substep'
+source ~/.zprofile
+pyenv install 3.11
+pyenv global 3.11
+message "Currently using Python (`python3 --version`) at \``which python3`\`" 'substep' 'info'
+
+message 'Upgrade pip3' 'step'
+message "Currently using pip (`pip3 --version`) at \``which pip3`\`" 'substep' 'info'
+pip3 install --upgrade pip
+message "Currently using pip (`pip3 --version`) at \``which pip3`\`" 'substep' 'info'
+
+message 'Install Python packages' 'step'
+pip3 install -r "$(dirname $0)/../apps/python/requirements.txt
