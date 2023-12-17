679218a3e47978f699622c06025a4b014188dd07
Kryštof Korb <krystof@korb.cz>
2023-12-17 23:17:44 +0100

    nicer ruby zprofie

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -7,7 +7,9 @@ source "$(dirname $0)/../utilities/message.sh"
 message 'Set up Ruby version manager' 'step'
 message "Currently using Ruby (`ruby --version`) at \``which ruby`\`" 'substep' 'info'
 message 'Add rbenv to ~/.zprofile' 'substep'
+echo '# Ruby version manager' >> ~/.zprofile
 echo 'eval "$(rbenv init -)"' >> ~/.zprofile
+echo '' >> ~/.zprofile
 message 'Reload .zprofile' 'substep'
 source ~/.zprofile
 rbenv install 3.2.2
