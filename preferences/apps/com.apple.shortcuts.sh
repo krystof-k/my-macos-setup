63d907a929f54714782cbdc855f5c9eaa71ef19b
Kryštof Korb <krystof@korb.cz>
2024-01-08 09:02:57 +0100

    Configure Shortcuts

---
+++ preferences/apps/com.apple.shortcuts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+message 'Configure Shortcuts' 'step'
+
+message 'Allow scripts' 'substep'
+message 'Manually allow running scripts' 'substep' 'to-do'
