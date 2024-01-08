3a51a9962226cb41824fd46c02042eb172444373
Kryštof Korb <krystof@korb.cz>
2024-01-08 08:19:39 +0100

    Configure Lunar

---
+++ preferences/apps/fyi.lunar.Lunar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # exit on any error
+
+source "$(dirname $0)/../../utilities/message.sh"
+
+message 'Configure Lunar' 'step'
+
+message 'Install CLI integration' 'substep'
+message 'Manually install CLI integration in settings' 'substep' 'to-do'
