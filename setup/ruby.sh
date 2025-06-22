c974c3f38c59d3cfb0bc85cdd72361d884555141
Kryštof Korb <krystof@korb.cz>
2025-06-22 10:03:08 +0200

    Bump to Ruby 3.4.4 (#21)

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -12,6 +12,6 @@ echo 'eval "$(rbenv init -)"' >> ~/.zprofile
 echo '' >> ~/.zprofile
 message 'Reload .zprofile' 'substep'
 source ~/.zprofile
-rbenv install 3.2.2
-rbenv global 3.2.2
+rbenv install 3.4.4
+rbenv global 3.4.4
 message "Currently using Ruby (`ruby --version`) at \``which ruby`\`" 'substep' 'info'
