d79a5c062464b9ee54d5adcb649b2cb170992db3
Kryštof Korb <krystof@korb.cz>
2026-04-14 11:18:40 +0200

    Fix Ghostty symlink (#62)

--- preferences/apps/com.mitchellh.ghostty.sh
+++ preferences/apps/com.mitchellh.ghostty.sh
@@ -11,8 +11,8 @@ if [[ -d /Applications/Ghostty.app ]]; then
   config_dir="$HOME/Library/Application Support/com.mitchellh.ghostty"
 
   message 'Symlink Ghostty config' 'substep'
-  mkdir -p "$(dirname "$config_dir")"
-  ln -sfn ~/Git/krystof-k/my-macos-setup/apps/ghostty "$config_dir"
+  mkdir -p "$config_dir"
+  ln -sf ~/Git/krystof-k/my-macos-setup/apps/ghostty/config.ghostty "$config_dir/config.ghostty"
 
   message 'Set Ghostty as default terminal' 'substep' 'to-do'
 fi
