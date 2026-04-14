becc136b1973f7758cbb0042592af0bf9289846f
Kryštof Korb <krystof@korb.cz>
2026-04-14 11:10:49 +0200

    Add Ghostty configuration (#60)

--- preferences/apps/com.mitchellh.ghostty.sh
+++ preferences/apps/com.mitchellh.ghostty.sh
@@ -2,11 +2,18 @@
 
 set -euo pipefail
 
+script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1091
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../utilities/message.sh"
+source "$script_directory/../../utilities/message.sh"
 
 if [[ -d /Applications/Ghostty.app ]]; then
   message 'Configure Ghostty' 'step'
+
+  config_dir="$HOME/Library/Application Support/com.mitchellh.ghostty"
+
+  message 'Copy Ghostty config' 'substep'
+  mkdir -p "$config_dir"
+  cp "$script_directory/../../apps/ghostty/config.ghostty" "$config_dir/config.ghostty"
 
   message 'Set Ghostty as default terminal' 'substep' 'to-do'
 fi
